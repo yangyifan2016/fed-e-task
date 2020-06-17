d1cccb9e0278b06107e7300c430235538fddb65f
YangYifan <15727366108@163.com>
2020-06-17 08:24:01 +0800

    gulpfile update

--- fed-e-task-02-01/code/pages-boilerplate/gulpfile.js
+++ fed-e-task-02-01/code/pages-boilerplate/gulpfile.js
@@ -1,7 +1,29 @@
 // 实现这个项目的构建任务
-const {src, dest} = require('gulp')
+const {src, dest, parallel, series} = require('gulp')
+const sass = require('gulp-sass')
+const babel = require('gulp-babel')
+const swig = require('gulp-swig')
 
-exports.default = () => {
-    return src('src/*.scss')
-    .pipe(dest('dist'))
+const style = () => {
+    return src('src/assets/styles/*.scss', { base: 'src' })
+        .pipe(sass())
+        .pipe(dest('dist'))
+}
+
+const script = () => {
+    return src('src/assets/scripts/*.js', { base: 'src' })
+        .pipe(babel({ presets: ['@babel/preset-env'] }))
+        .pipe(dest('dist'))
+}
+
+const page = () => {
+    return src('src/*.html', { base: 'src' })
+        .pipe(swig())
+        .pipe(dest('dist'))
+}
+
+const compile = parallel(style, script, page)
+
+module.exports = {
+    compile
 }
